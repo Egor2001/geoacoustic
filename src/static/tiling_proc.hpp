d86ce516574cd67d531a499719d65c69e24ed130
Egor2001 <elchinov.es@gmail.com>
2021-04-08 01:47:18 +0300

    diagonal tiling_proc revised

--- src/static/tiling_proc.hpp
+++ src/static/tiling_proc.hpp
@@ -159,7 +159,8 @@ struct RecursiveTile
 };
 
 template<typename TCell, int_t NTileRank>
-inline void tile_proc(int3_t idx3, const Config<TCell>& cfg, 
+inline __attribute__((always_inline)) 
+void tile_proc(int3_t idx3, const Config<TCell>& cfg, 
         VolumeSpan<TCell> ampl_next, VolumeSpan<TCell> ampl)
 {
     static constexpr int_t NTileSize = 1 << NTileRank;
@@ -230,6 +231,37 @@ inline void tile_proc(int3_t idx3, const Config<TCell>& cfg,
 }
 
 template<typename TCell, int_t NTileRank>
+inline __attribute__((always_inline)) 
+void diagonal_proc(int trd_idx, int trd_cnt, 
+        int_t cur_diag, int& cell_counter, const Config<TCell>& cfg, 
+        VolumeSpan<TCell> ampl_next, VolumeSpan<TCell> ampl)
+{
+    static constexpr int_t NTileSize = 1 << NTileRank;
+
+    int3_t dim3 = cfg.grid_size;
+    int_t min_z = std::max(static_cast<int_t>(0),
+            cur_diag - (dim3.x + dim3.y - 2 * NTileSize));
+    int_t max_z = std::min(dim3.z - NTileSize, cur_diag);
+    for (int_t iz = min_z; iz <= max_z; iz += NTileSize)
+    {
+        int_t min_y = std::max(static_cast<int_t>(0),
+                cur_diag - (iz + dim3.x - NTileSize));
+        int_t max_y = std::min(dim3.y - NTileSize, cur_diag - iz);
+        for (int_t iy = min_y; iy <= max_y; iy += NTileSize)
+        {
+            if (cell_counter % trd_cnt == trd_idx)
+            {
+                int_t ix = cur_diag - (iy + iz);
+                tile_proc<TCell, NTileRank>
+                    (int3_t{ix, iy, iz}, cfg, ampl_next, ampl);
+            }
+
+            ++cell_counter;
+        }
+    }
+}
+
+template<typename TCell, int_t NTileRank>
 void rectangular_tiling_proc(const Config<TCell>& cfg, 
         VolumeSpan<TCell> ampl_next, VolumeSpan<TCell> ampl)
 {
@@ -259,28 +291,11 @@ void diagonal_tiling_proc(const Config<TCell>& cfg,
         int trd_cnt = omp_get_num_threads();
         int trd_idx = omp_get_thread_num();
         int cur_cell_idx = 0;
-        for (int_t diag = diag_max; diag >= 0; diag -= NTileSize)
+        for (int_t cur_diag = diag_max; cur_diag >= 0; cur_diag -= NTileSize)
         {
-            int_t min_z = std::max(static_cast<int_t>(0), 
-                                   diag - (dim3.x + dim3.y - 2 * NTileSize));
-            int_t max_z = std::min(dim3.z - NTileSize, diag);
-            for (int_t iz = min_z; iz <= max_z; iz += NTileSize)
-            {
-                int_t min_y = std::max(static_cast<int_t>(0), 
-                                       diag - (iz + dim3.x - NTileSize));
-                int_t max_y = std::min(dim3.y - NTileSize, diag - iz);
-                for (int_t iy = min_y; iy <= max_y; iy += NTileSize)
-                {
-                    if (cur_cell_idx % trd_cnt == trd_idx)
-                    {
-                        int_t ix = diag - (iy + iz);
-                        tile_proc<TCell, NTileRank>
-                            (int3_t{ix, iy, iz}, cfg, ampl_next, ampl);
-                    }
-
-                    ++cur_cell_idx;
-                }
-            }
+            diagonal_proc<TCell, NTileRank>
+                (trd_idx, trd_cnt, cur_diag, cur_cell_idx, 
+                 cfg, ampl_next, ampl);
 
             // Synchronizing thread team before starting next diagonal
             #pragma omp barrier
@@ -297,32 +312,6 @@ void hyperplane_tiling_proc(const Config<TCell>& cfg,
     static constexpr int_t NDiagInterval = 5;
 
     int3_t dim3 = cfg.grid_size;
-
-#define PROC_DIAG_(CUR_DIAG, CELL_COUNTER) \
-    do { \
-        int_t local_diag = (CUR_DIAG); \
-        int_t min_z = std::max(static_cast<int_t>(0), \
-                local_diag - (dim3.x + dim3.y - 2 * NTileSize)); \
-        int_t max_z = std::min(dim3.z - NTileSize, (CUR_DIAG)); \
-        for (int_t iz = min_z; iz <= max_z; iz += NTileSize) \
-        { \
-            int_t min_y = std::max(static_cast<int_t>(0), \
-                    local_diag - (iz + dim3.x - NTileSize)); \
-            int_t max_y = std::min(dim3.y - NTileSize, local_diag - iz); \
-            for (int_t iy = min_y; iy <= max_y; iy += NTileSize) \
-            { \
-                if ((CELL_COUNTER) % trd_cnt == trd_idx) \
-                { \
-                    int_t ix = local_diag - (iy + iz); \
-                    tile_proc<TCell, NTileRank> \
-                        (int3_t{ix, iy, iz}, cfg, ampl_next, ampl); \
-                } \
-                \
-                ++(CELL_COUNTER); \
-            } \
-        } \
-    } while (false)
-
     int_t diag_max = dim3.x + dim3.y + dim3.z - 3 * NTileSize;
     int_t diag_min = diag_max - NDiagInterval * NTileSize;
 
@@ -341,7 +330,9 @@ void hyperplane_tiling_proc(const Config<TCell>& cfg,
                 for (int_t cur_diag = diag; cur_diag >= cur_min_diag; 
                      cur_diag -= NDiagInterval * NTileSize)
                 {
-                    PROC_DIAG_(cur_diag, cur_cell_idx);
+                    diagonal_proc<TCell, NTileRank>
+                        (trd_idx, trd_cnt, cur_diag, cur_cell_idx, 
+                         cfg, ampl_next, ampl);
                 }
 
                 #pragma omp barrier
@@ -355,7 +346,9 @@ void hyperplane_tiling_proc(const Config<TCell>& cfg,
             for (int_t cur_diag = diag; cur_diag >= cur_min_diag; 
                  cur_diag -= NDiagInterval * NTileSize)
             {
-                PROC_DIAG_(cur_diag, cur_cell_idx);
+                diagonal_proc<TCell, NTileRank>
+                    (trd_idx, trd_cnt, cur_diag, cur_cell_idx, 
+                     cfg, ampl_next, ampl);
             }
 
             #pragma omp barrier
